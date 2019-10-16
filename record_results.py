72fb7e84b995761f63a2e481c588c7fb80ca261f
Medeiros Luiz <medeirl1@kosh.org.aalto.fi>
2019-10-16 17:23:09 +0300

    deleting file

--- record_results.py
+++ record_results.py
@@ -1,20 +1,14 @@
-import sqlalchemy as sql
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import sys
 import os
+import argparse
 
-import scipy as sci
-import cvxpy as cp
-from cvxopt import matrix, solvers
-import sklearn.covariance as sk_cov
 from pdb import set_trace as bp
 
-home_dir = os.path.expandvars("$HOME")
-app_src_dir = '/dev/repos/classes/portfolio_research_project/BackTestingSystem'
-sys.path.insert(0, home_dir + app_src_dir)
-print(home_dir + app_src_dir)
+# home_dir = os.path.expandvars("$HOME")
+# app_src_dir = '/dev/repos/classes/portfolio_research_project/BackTestingSystem'
+# sys.path.insert(0, home_dir + app_src_dir)
+# print(home_dir + app_src_dir)
 
 
 
@@ -23,45 +17,85 @@ import StrategyResources as strat_res
 import pyRMT.pyRMT as rmt
 
 
+# Parse script arguments
+def parse_args(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--strategy", nargs='*', default=None,
+                        help="Strategy to be used.")
+    parser.add_argument("--assets", nargs='*', default=None,
+                        help="Number of assets to be used.")
+    parser.add_argument("--covariance", nargs='*', default=None,
+                        help="Covariance methods to be used. Use comma separated values.")
+    parser.add_argument("--lookback", nargs='*', default=None,
+                        help="Lookback period, in number of of days. Use comma separated values")
+    return parser.parse_args(args)
 
 
-start_date = "2000-01-01"
-end_date = '2019-01-01'
-test_container = None
-strategy_container = None
-result = None
+def main(args):
+    print(args)
+
+    start_date = "2000-01-01"
+    end_date = '2019-01-01'
+    test_container = None
+    strategy_container = None
+    result = None
 
 
-data = pd.read_pickle('data.pckl')
-table_names = data.columns
+    data = pd.read_pickle('data.pckl')
+    table_names = data.columns
+    # bp()
+    if args.strategy is not None:
+        weight_strategy_names = args.strategy
+    else:
+        weight_strategy_names = ['mdp_D', ]
+
+    if args.covariance is not None:
+        covariance_methods = args.covariance
+    else:
+        covariance_methods = ['sample', 'ledoit-wolf', 'RIE']
+    if args.assets is not None:
+        number_of_assets = []# args.assets
+        for asset in args.assets:
+            number_of_assets.append(int(asset))
+    else:
+        number_of_assets = [10, 50, 100, 150, 200]
+    if args.lookback is not None:
+        lookback_days = args.lookback
+        lookback_periods = []
+        for day in lookback_days:
+            lookback_periods.append(pd.DateOffset(days=int(day)))
+    else:
+        lookback_periods=[pd.DateOffset(months=6)]
+
+    lag_times = [pd.DateOffset(months=0)]
+    # q is quantity (number of shares)
+    # p is price
+    #commission_fn_a = lambda q, p: q * p * 0.002
+    commission_functions = [None]
+    strat_res.OPTIMUM_CONTAINER = {'date': [], 'value': []}
+    if test_container:
+        del test_container
+    if strategy_container:
+        del strategy_container
+    strategy_container, test_container = strat_res.build_test(number_of_assets,
+                    data,
+                    # optimum_container=optimum_container,
+                    covariance_methods=covariance_methods,
+                    weight_strategy_names=weight_strategy_names,
+                    commission_functions=commission_functions,
+                    lookback_periods=lookback_periods, lag_times=lag_times,
+                    add_random_strategy=False, add_one_over_n_strategy=False,
+                    )
+    if result:
+        del result
+    result = bt.run(*test_container)
+    strat_res.show_results(result, covariance_methods, test_container, show_return_graph=True,
+                           show_value_added_graph=True,
+                           show_optimum_graph=True,
+                           show_optimum_vs_period_graph=True)
 
 
-# GMVP Testing:
-weight_strategy_names = ['one_over_n']
-
-covariance_methods = ['ledoit-wolf']
-number_of_assets = [150]
-lookback_periods=[pd.DateOffset(days=1), pd.DateOffset(days=10), pd.DateOffset(days=20),
-                  pd.DateOffset(days=30), pd.DateOffset(days=60),pd.DateOffset(days=100),
-                  pd.DateOffset(days=180), pd.DateOffset(days=200),
-                  ]
-lag_times = [pd.DateOffset(months=0)]
-# q is quantity (number of shares)
-# p is price
-#commission_fn_a = lambda q, p: q * p * 0.002
-commission_functions = [None]
-strat_res.OPTIMUM_CONTAINER = {'date': [], 'value': []}
-strategy_container, test_container = strat_res.build_test(number_of_assets,
-                data,
-                # optimum_container=optimum_container,
-                covariance_methods=covariance_methods,
-                weight_strategy_names=weight_strategy_names,
-                commission_functions=commission_functions,
-                lookback_periods=lookback_periods, lag_times=lag_times,
-                add_random_strategy=False, add_one_over_n_strategy=False,)
-result = bt.run(*test_container)
-strat_res.show_results(result, covariance_methods, test_container, show_return_graph=True,
-                       show_value_added_graph=False,
-                       show_optimum_graph=True,
-                       show_optimum_vs_period_graph=True)
-
+# Entry point of the script
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
